62aabafdfa0e0f9e3a71ab7c487876e7adfebba8
Dax Marek Westerman <11837526+dax-westerman@users.noreply.github.com>
2026-02-01 06:51:58 -0600

    feat: Create `__init__.py` to conditionally expose Aspell and Jamspell spell checkers based on module availability.

--- neuspell/off_the_shelf/__init__.py
+++ neuspell/off_the_shelf/__init__.py
@@ -3,11 +3,11 @@ from .util import is_module_available
 __all__ = []
 
 if is_module_available("aspell"):
-    from .corrector_aspell import AspellChecker
+    from .corrector_aspell import AspellChecker  # noqa: F401
 
     __all__.extend(["AspellChecker"])
 
 if is_module_available("jamspell"):
-    from .corrector_jamspell import JamspellChecker
+    from .corrector_jamspell import JamspellChecker  # noqa: F401
 
     __all__.extend(["JamspellChecker"])
