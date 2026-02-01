3ab722775ae6d5741ef5b517f888a0268c7ed59a
Dax Marek Westerman <11837526+dax-westerman@users.noreply.github.com>
2026-01-31 19:59:43 -0600

    feat: Add multiple new sequence modeling architectures, pre-trained model download utilities, and a candidate reranking script.

--- neuspell/seq_modeling/bertsclstm.py
+++ neuspell/seq_modeling/bertsclstm.py
@@ -1,14 +1,27 @@
+from neuspell.seq_modeling.helpers import sclstm_tokenize
+from neuspell.seq_modeling.helpers import bert_tokenize_for_valid_examples
+import os
 import time
-
+import torch
+import tqdm
 from .downloads import download_pretrained_model
 from .evals import get_metrics
-from .helpers import *
+from .helpers import (
+    batch_iter,
+    get_model_nparams,
+    labelize,
+    untokenize_without_unks,
+    untokenize_without_unks2,
+)
 from .models import BertSCLSTM
 
 
 def load_model(vocab, verbose=False):
-    model = BertSCLSTM(3 * len(vocab["chartoken2idx"]), vocab["token2idx"][vocab["pad_token"]],
-                       len(vocab["token_freq"]))
+    model = BertSCLSTM(
+        3 * len(vocab["chartoken2idx"]),
+        vocab["token2idx"][vocab["pad_token"]],
+        len(vocab["token_freq"]),
+    )
 
     if verbose:
         print(model)
@@ -17,40 +30,57 @@ def load_model(vocab, verbose=False):
     return model
 
 
-def load_pretrained(model, checkpoint_path, optimizer=None, device='cuda'):
+def load_pretrained(model, checkpoint_path, optimizer=None, device="cuda"):
     if optimizer:
-        raise Exception("If you want optimizer, call `load_pretrained_large(...)` instead of `load_pretrained(...)`")
+        raise Exception(
+            "If you want optimizer, call `load_pretrained_large(...)` instead of `load_pretrained(...)`"
+        )
 
     if torch.cuda.is_available() and device != "cpu":
         map_location = lambda storage, loc: storage.cuda()
     else:
-        map_location = 'cpu'
+        map_location = "cpu"
     print(f"Loading model params from checkpoint dir: {checkpoint_path}")
 
     try:
-        checkpoint_data = torch.load(os.path.join(checkpoint_path, "pytorch_model.bin"), map_location=map_location)
+        checkpoint_data = torch.load(
+            os.path.join(checkpoint_path, "pytorch_model.bin"),
+            map_location=map_location,
+            weights_only=False,
+        )
     except FileNotFoundError:
         download_pretrained_model(checkpoint_path)
-        checkpoint_data = torch.load(os.path.join(checkpoint_path, "pytorch_model.bin"), map_location=map_location)
+        checkpoint_data = torch.load(
+            os.path.join(checkpoint_path, "pytorch_model.bin"),
+            map_location=map_location,
+            weights_only=False,
+        )
 
     model.load_state_dict(checkpoint_data)
 
     return model
 
 
-def load_pretrained_large(model, checkpoint_path, optimizer=None, device='cuda'):
+def load_pretrained_large(model, checkpoint_path, optimizer=None, device="cuda"):
     if torch.cuda.is_available() and device != "cpu":
         map_location = lambda storage, loc: storage.cuda()
     else:
-        map_location = 'cpu'
+        map_location = "cpu"
     print(f"Loading model params from checkpoint dir: {checkpoint_path}")
-    checkpoint_data = torch.load(os.path.join(checkpoint_path, "model.pth.tar"), map_location=map_location)
+    checkpoint_data = torch.load(
+        os.path.join(checkpoint_path, "model.pth.tar"),
+        map_location=map_location,
+        weights_only=False,
+    )
     # print(f"previously model saved at : {checkpoint_data['epoch_id']}")
 
-    model.load_state_dict(checkpoint_data['model_state_dict'])
+    model.load_state_dict(checkpoint_data["model_state_dict"])
     if optimizer is not None:
-        optimizer.load_state_dict(checkpoint_data['optimizer_state_dict'])
-    max_dev_acc, argmax_dev_acc = checkpoint_data["max_dev_acc"], checkpoint_data["argmax_dev_acc"]
+        optimizer.load_state_dict(checkpoint_data["optimizer_state_dict"])
+    max_dev_acc, argmax_dev_acc = (
+        checkpoint_data["max_dev_acc"],
+        checkpoint_data["argmax_dev_acc"],
+    )
 
     if optimizer is not None:
         return model, optimizer, max_dev_acc, argmax_dev_acc
@@ -61,13 +91,13 @@ def load_pretrained_large(model, checkpoint_path, optimizer=None, device='cuda')
 def model_predictions(model, data, vocab, device, batch_size=16):
     """
     model: an instance of BertSCLSTM
-    data: list of tuples, with each tuple consisting of correct and incorrect 
+    data: list of tuples, with each tuple consisting of correct and incorrect
             sentence string (would be split at whitespaces)
     """
 
     topk = 1
     # print("###############################################")
-    inference_st_time = time.time()
+    # inference_st_time = time.time()
     final_sentences = []
     VALID_batch_size = batch_size
     # print("data size: {}".format(len(data)))
@@ -76,11 +106,14 @@ def model_predictions(model, data, vocab, device, batch_size=16):
     model.to(device)
     for batch_id, (batch_labels, batch_sentences) in enumerate(data_iter):
         # set batch data for bert
-        batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits = bert_tokenize_for_valid_examples(
-            batch_labels, batch_sentences)
+        batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits = (
+            bert_tokenize_for_valid_examples(batch_labels, batch_sentences)
+        )
         if len(batch_labels_) == 0:
             print("################")
-            print("Not predicting the following lines due to pre-processing mismatch: \n")
+            print(
+                "Not predicting the following lines due to pre-processing mismatch: \n"
+            )
             print([(a, b) for a, b in zip(batch_labels, batch_sentences)])
             print("################")
             continue
@@ -90,7 +123,7 @@ def model_predictions(model, data, vocab, device, batch_size=16):
         # set batch data for others
         batch_labels_ids, batch_lengths = labelize(batch_labels, vocab)
         batch_idxs, batch_lengths_ = sclstm_tokenize(batch_sentences, vocab)
-        assert (batch_lengths_ == batch_lengths).all() == True
+        assert (batch_lengths_ == batch_lengths).all() is True
         assert len(batch_bert_splits) == len(batch_idxs)
         batch_idxs = [batch_idxs_.to(device) for batch_idxs_ in batch_idxs]
         # batch_lengths = batch_lengths.to(device)
@@ -98,11 +131,20 @@ def model_predictions(model, data, vocab, device, batch_size=16):
         # forward
         with torch.no_grad():
             """
-            NEW: batch_predictions can now be of shape (batch_size,batch_max_seq_len,topk) if topk>1, else (batch_size,batch_max_seq_len)
+            NEW: batch_predictions can now be of shape (batch_size,batch_max_seq_len,topk)
+                if topk>1, else (batch_size,batch_max_seq_len)
             """
-            _, batch_predictions = model(batch_idxs, batch_lengths, batch_bert_inp, batch_bert_splits,
-                                         targets=batch_labels_ids, topk=topk)
-        batch_predictions = untokenize_without_unks(batch_predictions, batch_lengths, vocab, batch_labels)
+            _, batch_predictions = model(
+                batch_idxs,
+                batch_lengths,
+                batch_bert_inp,
+                batch_bert_splits,
+                targets=batch_labels_ids,
+                topk=topk,
+            )
+        batch_predictions = untokenize_without_unks(
+            batch_predictions, batch_lengths, vocab, batch_labels
+        )
         final_sentences.extend(batch_predictions)
     # print("total inference time for this data is: {:4f} secs".format(time.time()-inference_st_time))
     return final_sentences
@@ -111,7 +153,7 @@ def model_predictions(model, data, vocab, device, batch_size=16):
 def model_inference(model, data, topk, device, batch_size=16, vocab_=None):
     """
     model: an instance of BertSCLSTM
-    data: list of tuples, with each tuple consisting of correct and incorrect 
+    data: list of tuples, with each tuple consisting of correct and incorrect
             sentence string (would be split at whitespaces)
     topk: how many of the topk softmax predictions are considered for metrics calculations
     """
@@ -120,23 +162,26 @@ def model_inference(model, data, topk, device, batch_size=16, vocab_=None):
     print("###############################################")
     inference_st_time = time.time()
     _corr2corr, _corr2incorr, _incorr2corr, _incorr2incorr = 0, 0, 0, 0
-    _mistakes = []
+    # _mistakes = []
     VALID_batch_size = batch_size
-    valid_loss = 0.
-    valid_acc = 0.
+    valid_loss = 0.0
+    # valid_acc = 0.0
     print("data size: {}".format(len(data)))
     data_iter = batch_iter(data, batch_size=VALID_batch_size, shuffle=False)
     model.eval()
     model.to(device)
     for batch_id, (batch_labels, batch_sentences) in tqdm(enumerate(data_iter)):
         torch.cuda.empty_cache()
-        st_time = time.time()
+        # st_time = time.time()
         # set batch data for bert
-        batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits = bert_tokenize_for_valid_examples(
-            batch_labels, batch_sentences)
+        batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits = (
+            bert_tokenize_for_valid_examples(batch_labels, batch_sentences)
+        )
         if len(batch_labels_) == 0:
             print("################")
-            print("Not predicting the following lines due to pre-processing mismatch: \n")
+            print(
+                "Not predicting the following lines due to pre-processing mismatch: \n"
+            )
             print([(a, b) for a, b in zip(batch_labels, batch_sentences)])
             print("################")
             continue
@@ -146,7 +191,7 @@ def model_inference(model, data, topk, device, batch_size=16, vocab_=None):
         # set batch data for others
         batch_labels_ids, batch_lengths = labelize(batch_labels, vocab)
         batch_idxs, batch_lengths_ = sclstm_tokenize(batch_sentences, vocab)
-        assert (batch_lengths_ == batch_lengths).all() == True
+        assert (batch_lengths_ == batch_lengths).all() is True
         assert len(batch_bert_splits) == len(batch_idxs)
         batch_idxs = [batch_idxs_.to(device) for batch_idxs_ in batch_idxs]
         # batch_lengths = batch_lengths.to(device)
@@ -155,13 +200,24 @@ def model_inference(model, data, topk, device, batch_size=16, vocab_=None):
         try:
             with torch.no_grad():
                 """
-                NEW: batch_predictions can now be of shape (batch_size,batch_max_seq_len,topk) if topk>1, else (batch_size,batch_max_seq_len)
+                NEW: batch_predictions can now be of shape (batch_size,batch_max_seq_len,topk)
+                    if topk>1, else (batch_size,batch_max_seq_len)
                 """
-                batch_loss, batch_predictions = model(batch_idxs, batch_lengths, batch_bert_inp, batch_bert_splits,
-                                                      targets=batch_labels_ids, topk=topk)
+                batch_loss, batch_predictions = model(
+                    batch_idxs,
+                    batch_lengths,
+                    batch_bert_inp,
+                    batch_bert_splits,
+                    targets=batch_labels_ids,
+                    topk=topk,
+                )
         except RuntimeError:
             print(
-                f"batch_idxs:{len(batch_idxs)},batch_lengths:{batch_lengths.shape},batch_bert_inp:{len(batch_bert_inp.keys())},batch_labels_ids:{batch_labels_ids.shape}")
+                (
+                    f"batch_idxs:{len(batch_idxs)},batch_lengths:{batch_lengths.shape},"
+                    f"batch_bert_inp:{len(batch_bert_inp.keys())},batch_labels_ids:{batch_labels_ids.shape}"
+                )
+            )
             raise Exception("")
         valid_loss += batch_loss
         # compute accuracy in numpy
@@ -169,18 +225,26 @@ def model_inference(model, data, topk, device, batch_size=16, vocab_=None):
         batch_lengths = batch_lengths.cpu().detach().numpy()
         # based on topk, obtain either strings of batch_predictions or list of tokens
         if topk == 1:
-            batch_predictions = untokenize_without_unks(batch_predictions, batch_lengths, vocab, batch_sentences)
+            batch_predictions = untokenize_without_unks(
+                batch_predictions, batch_lengths, vocab, batch_sentences
+            )
         else:
-            batch_predictions = untokenize_without_unks2(batch_predictions, batch_lengths, vocab, batch_sentences,
-                                                         topk=None)
+            batch_predictions = untokenize_without_unks2(
+                batch_predictions, batch_lengths, vocab, batch_sentences, topk=None
+            )
         # corr2corr, corr2incorr, incorr2corr, incorr2incorr, mistakes = \
         #    get_metrics(batch_labels,batch_sentences,batch_predictions,check_until_topk=topk,return_mistakes=True)
         # _mistakes.extend(mistakes)
         # batch_labels = [line.lower() for line in batch_labels]
         # batch_sentences = [line.lower() for line in batch_sentences]
         # batch_predictions = [line.lower() for line in batch_predictions]
-        corr2corr, corr2incorr, incorr2corr, incorr2incorr = \
-            get_metrics(batch_labels, batch_sentences, batch_predictions, check_until_topk=topk, return_mistakes=False)
+        corr2corr, corr2incorr, incorr2corr, incorr2incorr = get_metrics(
+            batch_labels,
+            batch_sentences,
+            batch_predictions,
+            check_until_topk=topk,
+            return_mistakes=False,
+        )
         _corr2corr += corr2corr
         _corr2incorr += corr2incorr
         _incorr2corr += incorr2corr
@@ -192,24 +256,38 @@ def model_inference(model, data, topk, device, batch_size=16, vocab_=None):
         del batch_labels, batch_lengths, batch_idxs, batch_lengths_, batch_bert_inp
         torch.cuda.empty_cache()
 
-        '''
+        """
         # update progress
         progressBar(batch_id+1,
                     int(np.ceil(len(data) / VALID_batch_size)), 
-                    ["batch_time","batch_loss","avg_batch_loss","batch_acc","avg_batch_acc"], 
+                    ["batch_time","batch_loss","avg_batch_loss","batch_acc","avg_batch_acc"],
                     [time.time()-st_time,batch_loss,valid_loss/(batch_id+1),None,None])
-        '''
+        """
     print(f"\nEpoch {None} valid_loss: {valid_loss / (batch_id + 1)}")
-    print("total inference time for this data is: {:4f} secs".format(time.time() - inference_st_time))
+    print(
+        "total inference time for this data is: {:4f} secs".format(
+            time.time() - inference_st_time
+        )
+    )
     print("###############################################")
     print("")
     # for mistake in _mistakes:
     #    print(mistake)
     print("")
-    print("total token count: {}".format(_corr2corr + _corr2incorr + _incorr2corr + _incorr2incorr))
     print(
-        f"_corr2corr:{_corr2corr}, _corr2incorr:{_corr2incorr}, _incorr2corr:{_incorr2corr}, _incorr2incorr:{_incorr2incorr}")
-    print(f"accuracy is {(_corr2corr + _incorr2corr) / (_corr2corr + _corr2incorr + _incorr2corr + _incorr2incorr)}")
+        "total token count: {}".format(
+            _corr2corr + _corr2incorr + _incorr2corr + _incorr2incorr
+        )
+    )
+    print(
+        (
+            f"_corr2corr:{_corr2corr}, _corr2incorr:{_corr2incorr}, "
+            f"_incorr2corr:{_incorr2corr}, _incorr2incorr:{_incorr2incorr}"
+        )
+    )
+    print(
+        f"accuracy is {(_corr2corr + _incorr2corr) / (_corr2corr + _corr2incorr + _incorr2corr + _incorr2incorr)}"
+    )
     print(f"word correction rate is {(_incorr2corr) / (_incorr2corr + _incorr2incorr)}")
     print("###############################################")
     return
